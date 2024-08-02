12b5f5516ed4e31eb3db8616b20eaebca99b6436
migarakavishan <migarakavishan43@gmail.com>
2024-08-02 21:31:58 +0530

    isnt complete

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,10 +1,26 @@
-// lib/todo.dart
 class ToDoModel {
+  String id;
   String title;
   bool isDone;
 
   ToDoModel({
+    this.id = '',
     required this.title,
     this.isDone = false,
   });
+
+  factory ToDoModel.fromJson(Map<String, dynamic> json) {
+    return ToDoModel(
+      id: json['id'],
+      title: json['title'],
+      isDone: json['isDone'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'isDone': isDone,
+    };
+  }
 }
